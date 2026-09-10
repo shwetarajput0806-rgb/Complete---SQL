25849bcdebfd3889ff5500bbf0cbc1d57cbda896
Shweta Rajput <shwetarajput0806@gmail.com>
2026-09-10 12:40:28 +0530

    Add files via upload

---
+++ day20.sql
@@ -0,0 +1,23 @@
+--Conditional function  -> CASE , COALESCE()
+
+SELECT * FROM products;
+
+--1.CASE function - categorizing based on condition
+/*
+WE eill categorize products into price ranges:
+Expensive if the price is greater than  or equal to 50,000.
+Moderate if the price is between 10,000 and 49,999.
+Affordable if the price is less than 10,000
+*/
+
+SELECT product_name , price,
+		CASE
+		   WHEN price >=50000 THEN 'Expensive'
+		   WHEN price >= 10000 AND price<= 49999 THEN 'Moderate'
+		   ELSE 'Affordable'
+		  END AS price_category
+FROM products;		  
+
+
+
+
